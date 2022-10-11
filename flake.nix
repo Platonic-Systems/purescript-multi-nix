e8cce87bf95ce11a224a44fec6e7722301ef0f18
Sridhar Ratnakumar <srid@srid.ca>
2022-10-11 16:31:31 -0400

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+
+    purs-nix.url = "github:purs-nix/purs-nix";
+    purs-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = [ "x86_64-linux" "x86_64-darwin" ];
+      perSystem = { self', system, pkgs, ... }: {
+        devShells.default = pkgs.mkShell {};
+      };
+    };
+}
